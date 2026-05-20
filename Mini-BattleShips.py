5c80a41edd14503dc3ba7c3baf3cbf2b63304b92
Altaaf8901 <Altaafhafiz16@yahoo.com>
2026-05-20 04:34:46 -0700

    Add display board function

--- Mini-BattleShips.py
+++ Mini-BattleShips.py
@@ -23,3 +23,24 @@ def Create_Board():
     
     return board
 
+#function outputs list into a 5 x 5 board
+#Row_num stores the count for what row is being printed
+#prints the columns numbered accordingly to the 5 x 5 board
+#loops through each row in board;
+#print is converting Row_number to a string and then puts spaces in between each word
+#Row_num is being added by 1 to increase count of which row from the list of boards is being printed
+def display_board(Visible_Board):
+    Row_Num = 1
+
+    print(' ', 1, 2, 3, 4, 5)
+    for row in Visible_Board:
+        print(str(Row_Num) + ' ' + ' '.join(row))
+        Row_Num += 1
+
+
+
+
+
+Visible_Board = Create_Board()
+Hidden_Board = Create_Board()
+display_board(Visible_Board)
