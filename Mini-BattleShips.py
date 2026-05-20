72ed941967657bdb2fc94dac6927c257c6a0dbac
Altaaf8901 <Altaafhafiz16@yahoo.com>
2026-05-20 04:04:00 -0700

    Add create board function

--- Mini-BattleShips.py
+++ Mini-BattleShips.py
@@ -1,3 +1,5 @@
+import random
+
 # Mini Battleship Terminal Game
 # Portfolio Project for Codecademy
 
@@ -8,3 +10,16 @@
 # - player guesses
 # - hit/miss logic
 # - a win/loss game loop
+
+#function creates a 5 x 5 board in a list
+#creates/stores an empty list
+#loops 5 times and append 'O' to list with 5 arrays being added
+#returns board in a single line list
+def Create_Board():
+    board = []
+
+    for row in range(5):
+        board.append(['O']*5)
+    
+    return board
+
