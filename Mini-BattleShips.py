3102670aa1177c5b84a80cdb687c2f9886efc25a
Altaaf8901 <Altaafhafiz16@yahoo.com>
2026-05-20 00:37:32 -0700

    Add initial Mini Battleship project file

---
+++ Mini-BattleShips.py
@@ -0,0 +1,10 @@
+# Mini Battleship Terminal Game
+# Portfolio Project for Codecademy
+
+# This project will use:
+# - a hidden board
+# - a visible board
+# - random ship placement
+# - player guesses
+# - hit/miss logic
+# - a win/loss game loop
